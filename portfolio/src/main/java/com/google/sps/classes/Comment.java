2e158dcc8998c9eec63ab4e2c12e76adef10eec3
Oscar David <oscardavidrodriguezm@gmail.com>
2020-06-25 03:15:39 +0000

    fetaure (Datastore implementation) Substitute in-memory comments storage for App Engine Datastore
    + Added timestamp attribute to Comment.java class to keep track of comments submissions.
    
    + Modified CommentsServlet.java servlet to store incoming comments into a DatastoreServiceFactory
    instead of a private List.
    
    + Added NewCommentServlet.java to register new comments from the client-side. The servlet registers
    the comments in the new Datastore factory.
    
    + Changed form action from /comments to /comment in index.html to point to the new NewCommentServlet.java
    servlet.
    
    *** No breaking changes ***

--- portfolio/src/main/java/com/google/sps/classes/Comment.java
+++ portfolio/src/main/java/com/google/sps/classes/Comment.java
@@ -3,10 +3,12 @@ package com.google.sps.classes;
 public class Comment {
     private String username;
     private String comment;
+    private long timestamp;
 
-    public Comment(String _username, String _comment) {
+    public Comment(String _username, String _comment, long _timestamp) {
         username = _username;
         comment = _comment;
+        timestamp = _timestamp;
     }
 
     public String getUsername() {
@@ -15,5 +17,9 @@ public class Comment {
 
     public String getComment() {
         return comment;
+    }
+
+    public long getTimeStamp() {
+        return timestamp;
     }
 }
--- portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -14,6 +14,12 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
@@ -27,39 +33,30 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/comments")
 public class CommentsServlet extends HttpServlet {
-    private List<Comment> comments;
-
-    @Override
-    public void init() {
-        comments = new ArrayList<>();
-    }
-
+    
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json;");
 
-        List<Comment> newest_comments = comments.subList(0, comments.size() > 5 ? 5 : comments.size());
+        Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery comments = datastore.prepare(query);
+
+        List<Comment> comments_to_set = new ArrayList<>();
+        for (Entity entity : comments.asIterable()) {
+            String username = (String) entity.getProperty("username");
+            String comment = (String) entity.getProperty("comment");
+            long timestamp = (long) entity.getProperty("timestamp");
+
+            Comment comment_to_set = new Comment(username, comment, timestamp);
+            comments_to_set.add(comment_to_set);
+        }
+
+        List<Comment> newest_comments = comments_to_set.subList(0, comments_to_set.size() > 5 ? 5 : comments_to_set.size());
         Gson gson = new Gson();
         String payload = gson.toJson(newest_comments);
 
-        response.getWriter().println(payload);
-    }
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json;");
-
-        String username, comment;
-        username = request.getParameter("username").length() > 0 ? request.getParameter("username") : "Anonymus";
-        comment = request.getParameter("comment");
-
-        System.out.println(username);
-        comments.add(0, new Comment(username, comment));
-
-        Gson gson = new Gson();
-        String payload = gson.toJson(comment);
-
-        response.sendRedirect("/index.html");
         response.getWriter().println(payload);
     }
 }

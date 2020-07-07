cdc1b3d44b1f89b798f39e279e9bd409a4cefa31
Oscar David <oscardavidrodriguezm@gmail.com>
2020-07-07 22:34:42 +0000

    feature (FindMeetingQuery.java) Construct FindMeetingQuery.java functionality to handle meeting scheduling.
    + Added Comparator function to Event.java to sort Event objects by start date.
    
    + Fill query function in FindMeetingQuery.java to search for potential timeslots to
    fit an incoming meeting request.
    
    + Added additional constructor to MeetingRequest.java to include optional_attendes
    if necessary to build test cases that included optional attendees.
    
    + Added requested test cases in FindMeetingQueryTest.java to test for different
    scenarios mentioned in the walkthrough.
    
    *** No breaking changes ***

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,89 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        int TIME_WINDOW_IN_MINUTES = 60 * 24;
+        Integer[] minutesNoOptAttendees = new Integer[TIME_WINDOW_IN_MINUTES];
+        Integer[] minutesOptAttendees = new Integer[TIME_WINDOW_IN_MINUTES];
+
+        Arrays.fill(minutesNoOptAttendees, 1);
+        Arrays.fill(minutesOptAttendees, 1);
+
+        ArrayList<TimeRange> availableTimeRanges = new ArrayList<>();
+        ArrayList<Event> eventsToSet = new ArrayList<>(events);
+
+        Set<String> attendees = new HashSet<String>(request.getAttendees());
+        Set<String> optionalAttendees = new HashSet<String>(request.getOptionalAttendees());
+
+        for (Event event : eventsToSet) { // N
+            /* Verify that at least one of the request attendees
+            is scheduled for the current event. */
+            Set<String> eventAttendees = event.getAttendees();
+            Boolean isAnyoneBusy = !Collections.disjoint(eventAttendees, attendees); // O + P
+            Boolean isAnyOptionalBusy = !Collections.disjoint(eventAttendees, optionalAttendees); // O + Q
+
+            TimeRange when = event.getWhen();
+            int start = when.start();
+            int end = when.start() + when.duration();
+
+            /* Mark available spaces as busy since there's at
+                least one person from the request that isn't available */
+            if (isAnyoneBusy) {
+                Arrays.fill(minutesNoOptAttendees, start, end, 0); // end - start ops.
+            }
+
+            if (isAnyoneBusy || isAnyOptionalBusy) {
+                Arrays.fill(minutesOptAttendees, start, end, 0); // end - start ops.
+            }
+        }
+
+        ArrayList<TimeRange> timeWindowsNoOptAttendees = getTimeWindows(minutesNoOptAttendees); // 1440
+        ArrayList<TimeRange> timeWindowsOptAttendees = getTimeWindows(minutesOptAttendees); // 1440
+
+        long duration = request.getDuration();
+        availableTimeRanges = getAvailableTimeRanges(timeWindowsOptAttendees, duration);
+
+        // If no time range works with optional attendees return the options without them.
+        return availableTimeRanges.size() == 0 ? 
+            getAvailableTimeRanges(timeWindowsNoOptAttendees, duration) : 
+            availableTimeRanges;
+    }
+
+    private ArrayList<TimeRange> getTimeWindows(Integer[] minutes) {
+        int windowSize = 0;
+        ArrayList<TimeRange> timeWindows = new ArrayList<>();
+        for (int i = 0; i < minutes.length; i++) {
+            if (minutes[i] == 1) {
+                ++windowSize;
+            } else {
+                if (windowSize != 0) {
+                    timeWindows.add(TimeRange.fromStartDuration(i - windowSize, windowSize));
+                    windowSize = 0;
+                }
+            }
+        }
+
+        timeWindows.add(TimeRange.fromStartDuration(minutes.length - windowSize, windowSize)); // Append last time slot
+
+        return timeWindows;
+    }
+
+    private ArrayList<TimeRange> getAvailableTimeRanges(ArrayList<TimeRange> timeWindows, long duration) {
+        ArrayList<TimeRange> timeRanges = new ArrayList<>();
+        for (TimeRange timeRange : timeWindows) {
+            /* If time window is large enough to contain the new meeting,
+            we have found a solution. */
+            if (timeRange.duration() >= duration)
+                timeRanges.add(timeRange);
+        }
+
+        return timeRanges;
+    }
 }
